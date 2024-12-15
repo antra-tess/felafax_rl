e603f1dd8336e61ee5859248d67031f11606a15c
Your Name <you@example.com>
2024-12-15 21:11:55 +0000

    chore: Update script to use start_train.sh instead of start_test.sh

--- start_all.sh
+++ start_all.sh
@@ -3,7 +3,7 @@ for i in {0..7}; do
   echo "Starting worker $i..."
   gcloud compute tpus tpu-vm ssh finetune-70b \
     --zone=us-central2-b \
-    --worker=$i -- 'bash -l -s' < start_test.sh 2>&1 | tee test_worker_${i}.log &
+    --worker=$i -- 'bash -l -s' < start_train.sh 2>&1 | tee test_worker_${i}.log &
 done
 
 # Wait for all workers to complete
